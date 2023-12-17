3c1319cd1450a0e3869a13dcebe25eafe174a0a5
abdalla114 <abdallasaleh503@gmail.com>
2023-12-17 15:24:10 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,85 @@
+var sName =document.getElementById('sitename');
+var url = document.getElementById('weburl');
+
+var inputsList = [];
+if(localStorage.getItem('inputs')!=null){
+    inputsList = JSON.parse(localStorage.getItem('inputs'))
+    display()
+}
+function main(){
+add();
+display();
+refill();
+}
+
+function add(){
+  if(validationName()&&validationSite()==true){
+    var inputs = {
+        name : sName.value,
+        link :url.value       
+    };
+    inputsList.push(inputs);
+    localStorage.setItem('inputs',JSON.stringify(inputsList))
+  }
+  else{
+    sName.classList.add('is-invalid');
+    url.classList.add('is-invalid')
+  }
+}
+
+function display(){
+    var holder = ``
+    for(var i = 0;i<inputsList.length;i++){
+        holder+=`
+        <tr>
+        <td class="py-3">${i+1}</td>
+        <td class="py-3">${inputsList[i].name}</td>
+        <td class="py-3"><a href="${inputsList[i].link}" class="text-white rounded-3 text-decoration-none  py-2 px-3 fw-semibold text-center visit-link" target="_blank">
+          <i class="fa-solid fa-eye"></i> Visit</a></td>
+          <td><button class="btn btn-danger rounded-3 fw-semibold " onclick='delItems(${i})'><i class="fa-solid fa-trash-can"></i>
+            Delete</button></td>
+      </tr>`
+    }
+    document.getElementById('bodyID').innerHTML = holder;
+}
+
+function refill(){
+    sName.value = '';
+    url.value = '';
+}
+
+function delItems(x){
+inputsList.splice(x,1)
+localStorage.setItem('inputs',JSON.stringify(inputsList))
+display()
+}
+
+function validationName(){
+    var regexname = /^\w{3,}(\s+\w+)*$/;
+    var text = sName.value;
+    if(regexname.test(text)){
+        sName.classList.add('is-valid')
+        sName.classList.remove('is-invalid')
+         return true
+    }
+    else{
+        sName.classList.remove('is-valid')
+        sName.classList.add('is-invalid')
+        return false
+    }
+}
+
+ function validationSite(){
+var regexSite =/^(https:\/\/www\.)[A-z]{1,}(\.com\/)$/;
+var site = url.value;
+if(regexSite.test(site)){
+    url.classList.add('is-valid')
+    url.classList.remove('is-invalid')
+     return true
+}
+else{
+    url.classList.remove('is-valid')
+    url.classList.add('is-invalid')
+    return false
+}
+ }
